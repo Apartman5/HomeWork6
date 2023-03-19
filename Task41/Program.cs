2c323136e3131283e46d9dc9c98b73e91cd724bc
Igor Kazakov <kiw@lefterova.ru>
2023-03-19 17:14:32 +0500

    first commit

---
+++ Task41/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Примеры:
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Prompt(string message)
+{
+    Console.WriteLine(message); // Выводим приглашение ко вводу
+    string readInput = Console.ReadLine(); // Вводим значение
+    int result = int.Parse(readInput); // приводим к числу
+    return result;
+}
+
+int[] InputArray (int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = Prompt($"введите {i + 1}-й элемент");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i =0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int PositiveNumbers(int[] array)
+{
+    int count = 0;
+    for(int i =0; i < array.Length; i++)
+    {
+       if(array[i] > 0) 
+       count++;
+    }
+    return count;
+}
+int length = Prompt("Задайте длину массива: ");// любое число
+int[] array = InputArray (length);
+PrintArray(array);
+Console.WriteLine($"В массиве {PositiveNumbers(array)} чисел больше нуля");
---
+++ Task43/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 43: 
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// Примеры:
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+const int COEFFICIENT = 0;
+const int CONSTANT = 1;
+const int XCOORD = 0;
+const int YCOORD = 1;
+const int LINE1 = 1;
+const int LINE2 = 2;
+
+double Prompt(string message)
+{
+    Console.Write(message); // Выводим приглашение ко вводу
+    string volue = Console.ReadLine(); // Вводим значение
+    double result = double.Parse(volue); // приводим к числу
+    return result;
+}
+
+double[] InputLineData(int numberOfLine)
+{
+    double[] lineData = new double[2];
+    lineData[COEFFICIENT] = Prompt($"Введите коэффициент для {numberOfLine} прямой > ");
+    lineData[CONSTANT] = Prompt($"Введите консанту для {numberOfLine} прямой > ");
+    return lineData;
+}
+
+bool ValidateLines(double[] lineData1, double[] lineData2)
+{
+    if (lineData1[COEFFICIENT] == lineData2[COEFFICIENT])
+    {
+        if (lineData1[CONSTANT] == lineData2[CONSTANT])
+        {
+            Console.WriteLine("Прямые совпадают");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Прямые параллельны");
+            return false;
+        }
+    } 
+    return true;
+}
+
+double[] lineData1 = InputLineData(LINE1);
+double[] lineData2 = InputLineData(LINE2);
+
+if (ValidateLines(lineData1, lineData2))
+{
+    double[] coord = FindCoords(lineData1, lineData2);
+    Console.Write($"Точка пересечение уравнений y={lineData1[COEFFICIENT]}*x+{lineData1[CONSTANT]} и y={lineData2[COEFFICIENT]}*x+{lineData2[CONSTANT]}");
+    Console.WriteLine($" имеет координаты ({coord[XCOORD]}, {coord[YCOORD]})");
+}
+
+double[] FindCoords(double[] lineData1, double[] lineData2)
+{
+    double[] coord = new double[2];
+    coord[XCOORD] = (lineData1[CONSTANT] - lineData2[CONSTANT]) / (lineData2[COEFFICIENT] - lineData1[COEFFICIENT]);
+    coord[YCOORD] = lineData1[CONSTANT] * coord[XCOORD] + lineData1[CONSTANT];
+    return coord;
+}
